ec6052ac7599325bff1440dc2885f74dead6f53a
alihusnain-dev <alihusnainmughal215@gmail.com>
2026-02-19 00:38:46 +0500

    confige app.js & add files

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set(express.static(path.join(__dirname,  'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.listen(port);
