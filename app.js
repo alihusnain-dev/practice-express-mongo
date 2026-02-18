36b3e95b5c34ea469e4cbade8003af60dda0ac2b
alihusnain-dev <alihusnainmughal215@gmail.com>
2026-02-19 00:47:22 +0500

    conffigures

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.set(express.static(path.join(__dirname,  'public')));
+app.use(express.static(path.join(__dirname,  'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
 app.listen(port);
